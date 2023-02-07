e4e5e18e96139f16c181b2e96e681db334739900
Diego Amicabile <diego.amicabile@gmail.com>
2023-02-07 21:57:55 +0100

    Other download projects

---
+++ add_extension.py
@@ -0,0 +1,31 @@
+import urllib.error
+
+from pytube import Playlist
+import os
+
+import os
+import yaml
+import time
+import argparse
+import shutil
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config')  # option that takes a value
+    args = parser.parse_args()
+    if args.config == None:
+        print("required argument --config <config>")
+    else:
+        with open(args.config, 'r') as confhandle:
+            conf_info = yaml.safe_load(confhandle)
+
+            playlist_id = conf_info["playlist_id"]
+            playlist_id = conf_info["playlist_id"]
+
+            target_dir = conf_info["target_dir"]
+            playlist_dir = os.path.join(target_dir, playlist_id)
+            for file_name in  os.listdir(playlist_dir):
+                if not file_name.endswith('.mp4'):
+                    src = os.path.join(playlist_dir, file_name)
+                    dest = os.path.join(playlist_dir, file_name+'.mp4')
+                    shutil.move(src, dest)
